06011da25c9d51389ad278e753c0d6bf845bfe96
MZiO <mziodev@gmail.com>
2024-12-11 16:05:33 +0100

    Initial Commit

---
+++ LeetCodeArrays101/LeetCodeArrays101App.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeArrays101App.swift
+//  LeetCodeArrays101
+//
+//  Created by MZiO on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeArrays101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
